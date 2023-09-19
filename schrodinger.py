c5f3ad44f70b98504b7b697759d4f1e0533680c2
Maren Backes <marbac@uni-bremen.de>
2023-09-19 13:55:44 +0200

    controlling and updating all files according to pylint configuration

--- schrodinger.py
+++ schrodinger.py
@@ -1,13 +1,14 @@
 """importing all necessary packages and modules"""
-from solvers import schrodinger
-from visualizer import visualizer
 from argparse import ArgumentParser
 import os
+from solvers import schrodinger
+from visualizer import visualizer
+
 
 parser = ArgumentParser(description='Change working directory')
 parser.add_argument("-d", "--directory", dest="directory", default= os.path.dirname(__file__), type=ascii)
 path = parser.parse_args()
-file_path = str(path.directory)[1:-1] + "schrodinger.inp"
-print(file_path)
-schrodinger(file_path)
-visualizer(file_path)
+FILE_PATH = str(path.directory)[1:-1] + "schrodinger.inp"
+print(FILE_PATH)
+schrodinger(FILE_PATH)
+visualizer(FILE_PATH)
